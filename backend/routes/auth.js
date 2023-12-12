dfcd09ed032e3685db3e8fd642bbcbf9e93fa13d
Archit Jain <architjain798@gmail.com>
2023-12-12 23:20:01 +0530

    Add the sign in and sign up api

---
+++ backend/routes/auth.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Signup
+router.post("/signup", async (req, res) => {
+  // Implement signup logic
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error during registration", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      "your-secret-key",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error("Error during login", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Logout
+router.post("/logout", (req, res) => {
+  try {
+    // Optional: You can add logic here to invalidate the user's token on the server side if necessary.
+    // For example, store a list of invalidated tokens in the database.
+
+    // Send a response indicating successful logout
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.error("Error during logout", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
